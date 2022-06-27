281ece6a3c668b7ef373cf735746646fb34a511a
Matthew J Muscat <matthew.muscat@mail.mcgill.ca>
2022-06-27 14:24:33 -0700

    commented fs_check

--- python_files/fs_check.py
+++ python_files/fs_check.py
@@ -6,9 +6,14 @@ from termcolor import colored
 
 
 def fs_checker(Data_name):
+    """
+    The main file system checker function, checks the data directory
+    to make sure the data folder is properly structured, checks for existence, emptiness
+    """
 
-    fsc_failed = False
+    fsc_failed = False # initially set failed variable to false
 
+    # access the path two levels up, this is where the data folder should be located relative to the main programme
     try:
         Path_two_levels_up = pathlib.Path(__file__).parents[2]
     except:
@@ -16,6 +21,7 @@ def fs_checker(Data_name):
     else:
         print("...Accessing structure two levels up...")
 
+    # Check to ensure a folder specified by the variable Data_name exists in the correct location
     dir_two_levels_up = [x for x in Path_two_levels_up.iterdir() if x.is_dir() == True]
     for p in dir_two_levels_up:
         if p.name == Data_name:
@@ -24,7 +30,8 @@ def fs_checker(Data_name):
         else:
             Data_exists = False
             pass
-
+    
+    # Handle the output of the data folder check
     if Data_exists == True:
         print('...The Data folder seems to exist in the correct place.')
     else:
@@ -32,6 +39,7 @@ def fs_checker(Data_name):
         fsc_failed = True
         return fsc_failed, fail_message
 
+    # Try joining the Data folder, I feel like this may throw an error if permissions are insufficient, but this try catch may be useless...
     try:
         data_folder = Path_two_levels_up.joinpath(Data_name)
     except:
@@ -39,7 +47,10 @@ def fs_checker(Data_name):
     else:
         print("...Accessing data folder...")
 
+    # check the data folder for its files and directories, the directories should be a list of patients, any loose files are ignored.
     num_data_directories, num_data_files, data_empty_message = test_empty(data_folder)
+    
+    # handle the output of the Data directory check
     if int(num_data_directories) == 0:
         fsc_failed = True
         fail_message = 'Your Data folder contains no patient folders'
@@ -50,6 +61,7 @@ def fs_checker(Data_name):
     elif int(num_data_directories) != 0 and int(num_data_files) == 0:
         print(data_empty_message)
 
+    # check each patient folder under Data, for any patient folders that are empty, stop the programme and tell the user
     patients_total_treatments = 0
     patients_total_loose_files = 0
     empty_patient = []
@@ -60,6 +72,7 @@ def fs_checker(Data_name):
         patients_total_loose_files = patients_total_loose_files + int(num_ptnt_files)
         patients_total_treatments = patients_total_treatments + int(num_ptnt_directories)
 
+    # handle the output of the patient folders check
     if len(empty_patient) != 0:
         fsc_failed = True
         fail_message = 'The following directories contain no treatment sub-folders:\n'+'\n'.join([str(x) for x in empty_patient])
@@ -69,7 +82,7 @@ def fs_checker(Data_name):
         if patients_total_loose_files != 0:
             print(colored('Warning:','yellow'), str(patients_total_loose_files)+ ' loose files found in treatment subfolders, these will be ignored!')
         
-
+    # If all other checks passed then return passed
     fsc_failed = False
     fail_message = 'FSC passed!'
     return fsc_failed, fail_message
