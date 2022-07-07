b84c190345494d434fb9e5149d210f5ae506e286
Matthew J Muscat <matthew.muscat@mail.mcgill.ca>
2022-07-07 16:25:03 -0700

    updated kd_tree file and now builds a tree, added a pca file for principal component analysis for fit of centroids, and installed sklearn package

--- python_files_dcm_meta_based/kD_data_tree_class.py
+++ python_files_dcm_meta_based/kD_data_tree_class.py
@@ -9,8 +9,33 @@ class Node(object):
         self.val = None
 
 
+def build_kdtree(vectors, node, dimension, max_dimension):
+    vals = sorted(vectors, key=lambda v: v[dimension])
+    if len(vals) == 1:
+        node.val = vals[0]
+        return node
+    if len(vals) == 2:
+        node.val = vals[1]
+        node.left = Node()
+        node.left.val = vals[0]
+        return node
+    mid = int((len(vals) - 1) / 2)
+    node.val = vals[mid]
+    node.left = build_kdtree(vals[:mid], Node(), (dimension+1) % max_dimension, max_dimension)
+    node.right = build_kdtree(vals[mid+1:], Node(), (dimension+1) % max_dimension, max_dimension)
+    return node
+
 def main():
-    print('This is the kd data tree programme, it is not meant to be run directly.')
+    #print('This is the kd data tree programme, it is not meant to be run directly.')
+
+    vecs = [[5,4],[2,3],[8,1],[9,6],[7,2],[4,7]]
+    sortedvecs = sorted(vecs, key=lambda v: v[0])
+    print(sortedvecs)
+    root = Node()
+    root1 = build_kdtree(vecs, root, 0, 1)
+    print(root1)
+    print('hello')
+
 
 if __name__ == '__main__':    
     main()
