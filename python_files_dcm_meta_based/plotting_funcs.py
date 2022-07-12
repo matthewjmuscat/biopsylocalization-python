6cdc635e6deb2893789d1eb64b1ba41752a7534c
Matthew J Muscat <matthew.muscat@mail.mcgill.ca>
2022-07-11 23:24:39 -0700

    created a structure reconstructor to recreate the structure along the best fit line, as a cylinder of constant radius along a best fit line, also made minor updates to main body of code to incorporate this, and updated plotting_funcs, the new package is called biopsy_creator

--- python_files_dcm_meta_based/plotting_funcs.py
+++ python_files_dcm_meta_based/plotting_funcs.py
@@ -1,5 +1,7 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
 
 def threeD_scatter_plotter(x,y,z):
     fig=plt.figure()
@@ -21,7 +23,7 @@ def arb_threeD_scatter_plotter(*data_and_color,**text):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     for data in data_and_color:
-        ax.scatter(data[0], data[1], data[2], c=data[3], marker='o')
+        ax.scatter(data[0], data[1], data[2], c=data[3], marker=data[4])
 
     iterator = 1
     for key, value in text.items():
@@ -29,9 +31,40 @@ def arb_threeD_scatter_plotter(*data_and_color,**text):
         y_pos=0.5-0.05*iterator
         ax.text2D(x_pos, y_pos, "%s: %s" % (key, value), transform=ax.transAxes)
         iterator = iterator + 1
+    set_axes_equal(ax)
+
     return fig
 
 def add_line(figure,line):
     ax = figure.get_axes()
     ax[0].plot(line[:, 0], line[:, 1], line[:, 2], 'g')
     return figure
+
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
