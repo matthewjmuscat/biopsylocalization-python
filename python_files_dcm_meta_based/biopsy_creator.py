fixed a bug with the rotation matrix function in math_funcs where an NaN matrix would be output if the two vectors to be aligned were pointing in the same direction. Now it returns the identity matrix.

python_files_dcm_meta_based/biopsy_creator.py
@@ -116,7 +116,11 @@ def biopsy_points_creater_by_transport(list_centroid_line_vector, list_origin_to
     norm_centroid_vector = np.linalg.norm(centroid_vector)
     lab_polar_centroid = np.arccos(centroid_vector[2]/norm_centroid_vector)
     norm_centroid_vector_xy_proj = norm_centroid_vector*np.sin(lab_polar_centroid)
-    lab_azimuth_centroid = np.arccos(centroid_vector[0]/norm_centroid_vector_xy_proj)
+    if norm_centroid_vector_xy_proj == 0:
+        lab_azimuth_centroid = 0
+    else:
+        lab_azimuth_centroid = np.arccos(centroid_vector[0]/norm_centroid_vector_xy_proj)
+    
     if centroid_vector[1] == 0 and centroid_vector[0] == 0:
         lab_azimuth_centroid = 0
     if centroid_vector[1] < 0:

python_files_dcm_meta_based/math_funcs.py
@@ -94,10 +94,14 @@ def rotation_matrix_from_vectors(vec1, vec2):
     """
     a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
     v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    # if the cross product is the zero vector, return the identity matrix as the rotation matrix
+    if np.array_equal(v, np.array([0,0,0]), equal_nan=False):
+        rotation_matrix = np.eye(3)
+    else:
+        c = np.dot(a, b)
+        s = np.linalg.norm(v)
+        kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+        rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
     return rotation_matrix
 
 